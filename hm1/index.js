f0a65c466dfa779090c24b32d104d31e01e40b8b
Vladimir Kliuev <vladimirkliuev@gmail.com>
2023-04-13 10:42:12 +0300

    hm1

---
+++ hm1/index.js
@@ -0,0 +1,17 @@
+var message = 'тут значение';
+var TextString = 'Тут значение String';
+var TextNumber = 123456;
+var numberBig = 1234563333333333333333333333333333n;
+var boolean = false;
+alert('Добро пожаловать');
+alert('У меня есть 4 переменных с разными типами данных');
+alert(TextString);
+alert(typeof TextString);
+alert(TextNumber);
+alert(typeof TextNumber);
+alert(numberBig);
+alert(typeof numberBig);
+alert(boolean);
+alert(typeof boolean);
+var answerUser = confirm('Понравился ли Вам сайт?');
+alert(answerUser);
